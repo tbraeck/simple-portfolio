f733729d0cf3088d7b705f376922425ac615924c
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-04-30 17:23:49 -0600

    styling for about section, incluin descriiption portion

--- src/AboutContext.js
+++ src/AboutContext.js
@@ -12,8 +12,9 @@ export const AboutProvider = ({ children }) => {
   // Define your About section data
   const aboutData = {
     title: "About Me",
-    description: "I am a software engineer passionate about building innovative solutions.",
-    techSkills: ["JavaScript", "React", "Node.js", "HTML", "CSS"]
+    description: "I am a results-driven software engineer with a fervent commitment to crafting cutting-edge solutions that drive innovation and exceed expectations. Armed with a robust skill set encompassing JavaScript, React, Ruby, Ruby on Rails, HTML, CSS, and WordPress, I am at the forefront of technological advancement. Leveraging my expertise in design and UI/UX, I architect solutions that seamlessly blend form and function, delivering intuitive user experiences that captivate and inspire. What sets me apart is not just my technical prowess, but also my mastery of essential soft skills. A skilled communicator and natural leader, I excel in fostering collaboration and driving team success. With a proven track record in effective management and problem-solving adaptability, I thrive in fast-paced environments where challenges are met with innovation. As a software engineer, I am poised to make a transformative impact, leveraging my blend of technical acumen and strategic soft skills to drive success in every endeavor.",
+    techSkills: ["JavaScript", "React", "Ruby", "Ruby on Rails", "HTML", "CSS", "WordPress", "Design", "UI/ UX"],
+    softSkills: ["Skilled Communicator", "Leadership", "Magagement", "Organization", "Problem-Solving/ Adaptability", "Collaboration"]
   };
 
   return (
